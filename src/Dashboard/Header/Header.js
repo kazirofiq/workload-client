3ce77c8e87fdf4f3de564e1167cc95a182300afd
kazirofiq <rofiqul01941056960@gmail.com>
2023-03-20 10:40:19 +0600

    crete siber logo

--- src/Dashboard/Header/Header.js
+++ src/Dashboard/Header/Header.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaElementor, FaSearch } from "react-icons/fa";
 import { MdForwardToInbox } from "react-icons/md";
 import { GrNotification } from "react-icons/gr";
 
 const Header = () => {
   return (
     <div className="flex gap-4 items-center justify-center">
-       <label htmlFor="toggle-sidebar" className=" flex lg:hidden  justify-center w-[100px] p-3 bg-white  rounded-xl">
-        <MdForwardToInbox className="text-4xl"></MdForwardToInbox>
+       <label htmlFor="toggle-sidebar" className=" flex lg:hidden  justify-center w-[100px] p-1 bg-white  rounded-xl">
+        <FaElementor className="text-4xl cursor-pointer"></FaElementor>
         
       </label>
-      <div className="flex flex-1 gap-10 items-center justify-center bg-white p-2 pl-3 rounded-xl">
-        <FaSearch className="mr-2 bg-white"></FaSearch>
+      <div className="flex flex-1 gap-2 items-center justify-center bg-white p-1 lg:p-3 rounded-xl">
+        <FaSearch className=" bg-white"></FaSearch>
         <input type="text" placeholder="Search" className="input  w-full " />
       </div>
-      <div className="p-3 bg-white w-[100px] rounded-xl">
+      <div className="p-1 lg:p-3 bg-white w-[100px] rounded-xl">
         <div className="indicator">
           <span className="indicator-item badge bg-[#6c51b0]">4</span>
           <button className=""><MdForwardToInbox className="text-4xl"></MdForwardToInbox></button>
         </div>
       </div>
-      <div className="p-3 bg-white w-[100px] rounded-xl">
+      <div className="p-1 lg:p-3 bg-white w-[100px] rounded-xl">
         <div className="indicator">
           <span className="indicator-item badge bg-[#6c51b0]">2</span>
           <button className=""><GrNotification className="text-4xl"></GrNotification></button>
--- src/Dashboard/LeftSideBar/LeftSideBar.js
+++ src/Dashboard/LeftSideBar/LeftSideBar.js
@@ -2,140 +2,87 @@ import React, { useContext } from "react";
 import { IoExitOutline, IoSettingsOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthProvider";
+import { AiOutlineHome } from "react-icons/ai";
+import { GrDocumentText } from "react-icons/gr";
+import { IoMdTime } from "react-icons/io";
+import { BsCalendar3 } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
 
 const LeftSideBar = () => {
-  const {logOut} = useContext(AuthContext);
-    const handleLogout = () =>{
-        logOut()
-        .then(() =>{})
-        .catch(err => console.log(err));
-    }
+  const { logOut } = useContext(AuthContext);
+  const handleLogout = () => {
+    logOut()
+      .then(() => {})
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="grid gap-8 grid-rows-[1fr_100px] w-full rounded-lg  mr-6 h-screen mt-6 ml-6  ">
-      
       <div className=" bg-white w-full  rounded-lg">
         <ul className="menu bg-white p-2 rounded-box">
           <li>
-            <Link to='/dashboard'>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
-              Dashboard
-              </Link>
+            <Link to="/dashboard">
+              <div className="flex gap-4 items-center">
+                <AiOutlineHome className="text-xl"></AiOutlineHome>
+                <h2>Dashboard</h2>
+              </div>
+            </Link>
           </li>
           <li>
             <Link to="/dashboard/document">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              Document
-              
-              </Link>
+              <div className="flex gap-4 items-center">
+                <GrDocumentText className="text-xl"></GrDocumentText>
+                <h2>Document</h2>
+              </div>
+            </Link>
           </li>
           <li>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Calender
-            </a>
+          <Link to="/dashboard/calender">
+              <div className="flex gap-4 items-center">
+              <IoMdTime className="text-xl"></IoMdTime>
+                <h2>Calender</h2>
+              </div>
+            </Link>
           </li>
           <li>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Activity
-            </a>
+          <Link to="/dashboard/activity">
+              <div className="flex gap-4 items-center">
+                <BsCalendar3 className="text-xl"></BsCalendar3>
+                <h2>Activity</h2>
+              </div>
+            </Link>
           </li>
           <li>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Profile
-            </a>
+          <Link to="/dashboard/profile">
+              <div className="flex gap-4 items-center">
+              <CgProfile className="text-xl"></CgProfile>
+                <h2>Profile</h2>
+              </div>
+            </Link>
           </li>
         </ul>
+        {/* divider */}
         <div className="divider p-10 bg-white"></div>
+        {/* profile */}
         <ul className="menu bg-white  p-2 rounded-box">
           <li>
             <div className="flex items-center">
-              
               <input type="checkbox" className="toggle text-sm" checked />
-               <h2>Darkmode</h2>
-              
+              <h2>Darkmode</h2>
             </div>
           </li>
           <li>
-          <div className="flex gap-4 items-center">
-              
-              <IoSettingsOutline className="text-4xl"></IoSettingsOutline>
-               <h2>Settings</h2>
-              
+            <div className="flex gap-4 items-center">
+            <IoExitOutline className="text-4xl"></IoExitOutline>
+              <h2>Settings</h2>
             </div>
           </li>
           <li>
-          <div onClick={handleLogout} className="flex items-center text-red-600">
-              
-          <IoExitOutline className="text-4xl"></IoExitOutline >
-               <h2>Log Out</h2>
-              
+            <div
+              onClick={handleLogout}
+              className="flex items-center text-red-600"
+            >
+              <IoExitOutline className="text-4xl"></IoExitOutline>
+              <h2>Log Out</h2>
             </div>
           </li>
         </ul>
